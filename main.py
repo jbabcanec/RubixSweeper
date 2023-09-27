f0e9d8f4a1592e3cd80763fed4556ecd47c7011f
jbabcanec <joseph.babz@gmail.com>
2023-09-27 13:31:30 -0400

    win/lose and flood fill features added

--- main.py
+++ main.py
@@ -21,20 +21,25 @@ pygame.display.set_caption("Rubix Minesweeper")
 clock = pygame.time.Clock()
 
 # Initialize a Rubix instance with dimensions
-size = 3  # For a 3x3 face
-mines = 4  # Number of mines on each face
+size = 6 
+mines = 4
 rubix = Rubix(size=size, mines=mines)
 
 # Main game loop
 running = True
 while running:
     screen.fill(BLACK)  # Clear the screen
+
+    front_face_minesweeper = rubix.faces['front']
+    game_over = front_face_minesweeper.game_over 
+    game_win = front_face_minesweeper.check_win()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
         # Mouse click events
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN and not game_over:
             mouse_x, mouse_y = event.pos
             grid_x, grid_y = (mouse_x - 100) // CUBE_SIZE, (mouse_y - 100) // CUBE_SIZE
 
@@ -43,12 +48,11 @@ while running:
                 if event.button == 1:  # Left click to reveal
                     rubix.reveal_cell('front', grid_x, grid_y)
                 elif event.button == 3:  # Right click to flag
-                    rubix.flag_cell('front', grid_x, grid_y)  # flag_cell method needs to be implemented in Rubix
+                    rubix.flag_cell('front', grid_x, grid_y)
                     print(f"After flag_cell call, flagged status: {rubix.get_flagged('front')}")
 
-    
     # Dynamic Grid Drawing
-    x, y = 100, 100  # Starting position for drawing the "front" face
+    x, y = 100, 100
     front_face = rubix.get_face('front')
     front_revealed = rubix.get_revealed('front')
     front_flagged = rubix.get_flagged('front')
@@ -57,17 +61,50 @@ while running:
         for j in range(size):
             pygame.draw.rect(screen, WHITE, (x + i * CUBE_SIZE, y + j * CUBE_SIZE, CUBE_SIZE, CUBE_SIZE), 1)
             
-            if front_revealed[i][j]:  # Check if this cell is revealed
+            # If cell is revealed, fill it with a light gray color
+            if front_revealed[i][j]:
+                pygame.draw.rect(screen, (200, 200, 200), (x + i * CUBE_SIZE + 1, y + j * CUBE_SIZE + 1, CUBE_SIZE - 1, CUBE_SIZE - 1))
+                
+                # Draw the number or mine
                 font = pygame.font.SysFont(None, 36)
                 text_surface = font.render(str(front_face[i][j]), True, GREEN)
                 screen.blit(text_surface, (x + i * CUBE_SIZE + 15, y + j * CUBE_SIZE + 15))
-            
-            elif front_flagged[i][j]:  # Check if this cell is flagged
+
+            # If cell is flagged, draw a red X
+            elif front_flagged[i][j]:
                 pygame.draw.line(screen, RED, (x + i * CUBE_SIZE + 10, y + j * CUBE_SIZE + 10), (x + i * CUBE_SIZE + 40, y + j * CUBE_SIZE + 40), 3)
                 pygame.draw.line(screen, RED, (x + i * CUBE_SIZE + 40, y + j * CUBE_SIZE + 10), (x + i * CUBE_SIZE + 10, y + j * CUBE_SIZE + 40), 3)
 
-        
+    if game_over:
+        font = pygame.font.SysFont(None, 48)
+        lose_surface = font.render("You Lose", True, RED)
+        screen.blit(lose_surface, (SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT // 2 - 25))
+
+        pygame.draw.rect(screen, GREEN, (SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT // 2 + 50, 130, 50))
+        reset_surface = font.render("Reset", True, BLACK)
+        screen.blit(reset_surface, (SCREEN_WIDTH // 2 - 30, SCREEN_HEIGHT // 2 + 60))
+
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        if (SCREEN_WIDTH // 2 - 50 <= mouse_x <= SCREEN_WIDTH // 2 + 50) and (SCREEN_HEIGHT // 2 + 50 <= mouse_y <= SCREEN_HEIGHT // 2 + 100):
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                rubix = Rubix(size=size, mines=mines)
+
+    elif game_win:  # New block for game win
+        font = pygame.font.SysFont(None, 48)
+        win_surface = font.render("You Win", True, GREEN)
+        screen.blit(win_surface, (SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT // 2 - 25))
+
+        pygame.draw.rect(screen, GREEN, (SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT // 2 + 50, 130, 50))
+        reset_surface = font.render("Reset", True, BLACK)
+        screen.blit(reset_surface, (SCREEN_WIDTH // 2 - 30, SCREEN_HEIGHT // 2 + 60))
+
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        if (SCREEN_WIDTH // 2 - 50 <= mouse_x <= SCREEN_WIDTH // 2 + 50) and (SCREEN_HEIGHT // 2 + 50 <= mouse_y <= SCREEN_HEIGHT // 2 + 100):
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                rubix = Rubix(size=size, mines=mines)
+
     pygame.display.flip()
     clock.tick(60)
 
 pygame.quit()
+
--- minesweeper.py
+++ minesweeper.py
@@ -7,14 +7,24 @@ class Minesweeper:
         self.board = [[' ' for _ in range(size)] for _ in range(size)]
         self.revealed = [[False for _ in range(size)] for _ in range(size)]
         self.flagged = [[False for _ in range(size)] for _ in range(size)]
-        self.place_mines()
-        self.calculate_numbers()
+        self.first_click = True
+        self.game_over = False
 
-    def place_mines(self):
+    def place_mines(self, exclude_row, exclude_col):
         mine_positions = random.sample(range(self.size * self.size), self.mines)
         for pos in mine_positions:
             row, col = divmod(pos, self.size)
+            if row == exclude_row and col == exclude_col:
+                continue
             self.board[row][col] = 'M'
+
+    def check_win(self):
+        for row in range(self.size):
+            for col in range(self.size):
+                if not self.revealed[row][col] and self.board[row][col] != 'M':
+                    return False
+        return True
+
 
     def calculate_numbers(self):
         for row in range(self.size):
@@ -30,16 +40,49 @@ class Minesweeper:
                                 count += 1
                 self.board[row][col] = str(count) if count > 0 else ' '
 
+    def debug_output(self):
+        print("Board:")
+        for row in self.board:
+            print(row)
+        print("Revealed:")
+        for row in self.revealed:
+            print(row)
+        print("Flagged:")
+        for row in self.flagged:
+            print(row)
+
     def reveal_cell(self, row, col):
-        """
-        Reveal a cell on the Minesweeper board.
-        """
-        self.revealed[row][col] = True  # Sets the cell as revealed
+        if self.first_click:
+            self.place_mines(row, col)  # Place mines, excluding the first clicked cell
+            self.calculate_numbers()
+            self.first_click = False  # Set first_click to False
+
+        if self.game_over:
+            return
+
+        if self.revealed[row][col]:
+            return
+
+        self.revealed[row][col] = True  # Set the cell as revealed
+
+        if self.board[row][col] == 'M':
+            self.game_over = True  # Game is over if a mine is revealed
+            return
+
+        if self.board[row][col] == ' ':
+            for dx in [-1, 0, 1]:
+                for dy in [-1, 0, 1]:
+                    new_row, new_col = row + dx, col + dy
+                    if 0 <= new_row < self.size and 0 <= new_col < self.size:
+                        if not self.revealed[new_row][new_col]:
+                            self.reveal_cell(new_row, new_col)
 
     def flag_cell(self, row, col):
-        """
-        Toggle a flag on a cell on the Minesweeper board.
-        """
-        if not self.revealed[row][col]:  # Check if the cell is not revealed
-            self.flagged[row][col] = not self.flagged[row][col]  # This line toggles the flag
-            print(f"Flagged status of cell ({row}, {col}): {self.flagged[row][col]}")
+        print("Before flag_cell:")
+        self.debug_output()
+
+        if not self.revealed[row][col]:
+            self.flagged[row][col] = not self.flagged[row][col]
+
+        print("After flag_cell:")
+        self.debug_output()
